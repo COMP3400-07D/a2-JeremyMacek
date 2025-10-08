77e468dc8bcf0b2409aa5d85d2497ebc02bd5c2b
Jeremy Macek <macekjeremy@kali.os>
2025-10-08 12:46:58 -0400

    Completed string concat

--- string_concat.c
+++ string_concat.c
@@ -4,5 +4,23 @@
  * TODO: Describe what the function does
  */
 void string_concat(char* src1, int src1_cap, const char* src2) {
+
+        int i = 0;
+        int j = 0;
+        if (src1 != NULL){
+            while (src1[i] != '\0'){
+                i++;
+            }
+        }
+        
+
+        if (src2 != NULL){
+            while (src2[j] != '\0' && i < (src1_cap - 1)){
+                src1[i] = src2[j];
+                i++;
+                j++;
+            }
+            src1[i] = '\0';
+        }
 }
 
